f7ff96672809a2fcb04202af5a55ba3f85892d6a
sudharsan <sudharsan@sudharsans-MacBook-Pro.local>
2026-09-11 11:58:04 +0530

    search products commited

--- src/app/products/fetch-products/fetch-products.component.ts
+++ src/app/products/fetch-products/fetch-products.component.ts
@@ -1,8 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProductService } from '../product.service';
 import { AuthService } from '../../auth/auth.service';
-
 
 @Component({
   standalone: true,
@@ -16,6 +15,24 @@ export class FetchProductsComponent {
 
   private productService = inject(ProductService);
   public authService = inject(AuthService);
-  productResource = this.productService.productResource;
+  //productResource = this.productService.productResource;
+  searchText = signal("");
 
+  productResource = this.productService.searchProductResource({searchText: this.searchText});
+
+  OnSearchInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.searchText.set(value);
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const triggerPosition = document.documentElement.scrollHeight - 200;
+
+    if (scrollPosition >= triggerPosition) {
+      console.log("bottom reached");
+    }
+}
+  
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, resource, Signal } from '@angular/core';
 import type { CreateProductRequest } from './product.model';
 import { Product, ProductApiResponse } from './product.model';
-import { firstValueFrom } from 'rxjs';
+import { debounceTime, firstValueFrom, of, switchMap, map, distinctUntilChanged } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Injectable({ providedIn: 'root' })
 
 export class ProductService {
     private http = inject(HttpClient);
 
-    readonly productResource = resource<Product[], void>({
-        defaultValue: [],
-        loader: () => this.fetchProducts()
-    });
+    // readonly productResource = resource<Product[], void>({
+    //     defaultValue: [],
+    //     loader: () => this.fetchProducts()
+    // });
 
     productDetailResource({ productId }: { productId: Signal<number | null>; }) {
         return resource<Product | null, { productId: number } | undefined>({
@@ -31,6 +32,27 @@ export class ProductService {
         });
     }
 
+    searchProductResource({searchText}: {searchText: Signal<string>}) {
+        return rxResource<Product[], string>({
+            defaultValue: [],
+            params: () => searchText(),
+            stream: ({ params }) =>
+                of(params).pipe(
+                    debounceTime(500),
+                    distinctUntilChanged(),
+                    switchMap((searchValue) => {
+                        const query = searchValue.trim();
+                        const endpoint = query
+                            ? `https://jsonexamples.com/products/search?q=${query}`
+                            : `https://jsonexamples.com/products?limit=12`;
+                        return this.http
+                            .get<ProductApiResponse>(endpoint)
+                            .pipe(map((response) => response.products));
+                    })
+                )
+        });
+    }
+
     addProduct({ payload }: { payload: Signal<CreateProductRequest | null>; }) {
         return resource<unknown | null, CreateProductRequest | undefined>({
             defaultValue: null,
@@ -44,12 +66,12 @@ export class ProductService {
         });
     }
 
-    private async fetchProducts() {
-        const data = await firstValueFrom(
-            this.http.get<ProductApiResponse>('https://jsonexamples.com/products?limit=12')
-        );
-        return data.products;
-    }
+    // private async fetchProducts() {
+    //     const data = await firstValueFrom(
+    //         this.http.get<ProductApiResponse>('https://jsonexamples.com/products?limit=12')
+    //     );
+    //     return data.products;
+    // }
 
     private async fetchProduct(productId: number) {
         return firstValueFrom(this.http.get<Product>(`https://jsonexamples.com/products/${productId}`));
